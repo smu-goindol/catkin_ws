72c11d478ed55303f490b21380f88403010732ab
Hepheir <hepheir@gmail.com>
2024-05-22 01:13:56 +0900

    chore: update init.sh (chmod +x, add -y for apt install)

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
-apt install curl
-apt install lsb_release
+apt install -y curl
+apt install -y lsb_release
 
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 apt update
-apt install ros-noetic-desktop-full
+apt install -y ros-noetic-desktop-full
 pip install -r requirements.txt
 
 echo "export ROS_MASTER_URI=http://localhost:11311" >> ~/.bashrc
