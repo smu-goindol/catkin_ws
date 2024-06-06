731efa55f16765399fc6034539d77174bf96cb20
ssong072413 <149565823+ssong072413@users.noreply.github.com>
2024-06-06 15:35:23 +0900

    주석 업데이트

--- src/assignment_1/src/goindol/path.py
+++ src/assignment_1/src/goindol/path.py
@@ -8,51 +8,64 @@ from .point import Point
 
 
 class Path:
+    ## 곡선의 길이 반환.
     def __len__(self) -> int:
         """Return the length of the curve."""
         ...
-
+        
+    ## 곡선을 순회하는 iterator 반환.
     def __iter__(self) -> Iterable:
         """Return an iterator over the points of the curve."""
         ...
-
-    def get_points(self) -> np.ndarray:
+        
+    ## 곡선의 모든 점들을 numpy 배열로 반환.
+    def get_points(self) -> np.ndarray:        
         """Return the points of the curve."""
         ...
-
+        
+    ## 곡선의 x좌표를 리스트로 반환.
     def get_x_points(self) -> List[float]:
         """Return the x-coordinates of the points of the curve."""
         ...
-
-    def get_y_points(self) -> List[float]:
+        
+    ## 곡선의 y좌표를 리스트로 반환.
+    def get_y_points(self) -> List[float]:       
         """Return the y-coordinates of the points of the curve."""
         ...
-
+        
+    ## pygame 화면에 곡선 그리기.
     def draw(self, surface: pygame.Surface) -> None:
         """Draw the curve on the surface."""
         ...
 
 
 class AbstractPath(Path):
+    ## 주어진 점들을 사용하여 경로 초기화.
     def __init__(self, points: np.ndarray):
         self._points = points
 
+    ## 연결된 점들의 거리의 합인 경로의 길이 반환.
     def __len__(self) -> int:
         return np.sum(np.sqrt(np.sum(np.diff(self.get_points(), axis=0)**2, axis=1)))
 
+    ## 경로를 순회하며 각 반복에서 x,y좌표 반환.
     def __iter__(self) -> Iterable:
         for x, y in self.get_points():
             yield x, y
 
+    ## 경로를 구성하는 점들의 배열 반환.
     def get_points(self) -> np.ndarray:
         return self._points
 
+    ## 경로의 x좌표 반환.
     def get_x_points(self) -> List[float]:
         return self._points[:, 0].tolist()
 
+    ## 경로의 y좌표 반환.
     def get_y_points(self) -> List[float]:
         return self._points[:, 1].tolist()
 
+    ## pygame 화면에서 경로는 초록색 선으로 표시됨.
     def draw(self, surface: pygame.Surface) -> None:
         LINE_COLOR = (0, 255, 0)
         pygame.draw.lines(surface, LINE_COLOR, False, self.get_points(), width=2)
@@ -61,13 +74,16 @@ class AbstractPath(Path):
 class BezierCurve(AbstractPath):
     def __init__(self, *points: Point, samples: int = 1000):
         P = np.array(points)
-        T = np.linspace(0, 1, samples) # sample points
-        N = P.shape[0] - 1  # Degree of the Bezier curve (number of control points - 1)
-        M = T.shape[0]  # Number of parameter values
-        # Create an array to hold the points on the Bezier curve
-        bezier_curve = np.zeros((M, 2))
-        # Calculate the Bezier curve points
+        T = np.linspace(0, 1, samples) # 0부터 1까지의 sample points 생성.
+        N = P.shape[0] - 1  # 제어점들의 개수에서 1을 빼 베지어 곡선의 차수 계산. # Degree of the Bezier curve (number of control points - 1)
+        M = T.shape[0]  # 매개변수 값의 개수 계산. # Number of parameter values
+        bezier_curve = np.zeros((M, 2)) #베지어 곡선 상의 점들을 저장할 배열 생성. # Create an array to hold the points on the Bezier curve
+
+        #==========================================
+        # 베지어 곡선 계산. # Calculate the Bezier curve points
+        # 제어점의 개수에 따라 반복하여 제어점에 대한 가중치를 계산한 후 새로운 축에 대한 배열 반환.
+        #==========================================
         for i in range(N + 1):
             term = (comb(N, i) * (1 - T) ** (N - i) * T ** i)[:, np.newaxis]
             bezier_curve += term * P[i]
-        super().__init__(bezier_curve)
+        super().__init__(bezier_curve) # 곡선을 나타내는 점들의 배열로 초기화.
